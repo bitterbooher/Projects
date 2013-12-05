e6b2c25516715109006015425ce5f6cb6938b5ff
bitterbooher <qbooher@gmail.com>
2013-12-04 22:03:15 -0700

    this will work for  for questions and answers in order, but it isn't randomly taking questions from the question page - it is taking all of them.
    what about matching the answers from the key to the questions.

--- Quiz Maker/qm.py
+++ Quiz Maker/qm.py
@@ -1,14 +1,14 @@
 #quiz maker
 
 #how to read in files
-
+x = 0
 useranswers = []
 realanswers = []
 with open("sample1.txt") as f:
 	for line in f:
 		print line,
 		var = raw_input("Your answer :")
-		useranswers.append(var)
+		useranswers.append(var.lower())
 #print useranswers
 
 with open("key1.txt") as b:
@@ -17,12 +17,17 @@ with open("key1.txt") as b:
 		realanswers.append(line.rstrip()) #rstrip will rid of \n 
 print realanswers
 
+"""
 print useranswers[0] == realanswers[0]
 print useranswers[1] == realanswers[1]
 print useranswers[2] == realanswers[2]
+
+"""
 #print key versus answers
 
-
+while x < len(useranswers):
+	print useranswers[x] == realanswers[x]
+	x+= 1
 #answer each question in line
 
 #randomly select questions
